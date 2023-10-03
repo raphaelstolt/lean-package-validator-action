e1d0b80937fb4b6a8a6bf528120e921735b4ef7f
Raphael Stolt <raphael.stolt@gmail.com>
2023-10-03 09:56:09 +0200

    Improves lpv-runner

--- bin/lpv-runner.php
+++ bin/lpv-runner.php
@@ -2,32 +2,68 @@
 <?php
 exec("lpv --version", $output, $existenceStatusCode);
 
+$options = getopt('h', ['directory:', 'help']);
+$verbose = false;
+
+$help = <<<HELP
+This command delegates the GitHub action to the lpv binary:
+
+Usage:
+  lpvrunner.php [-hv] [--help|--directory=<directory-to-scan>]
+
+Options:
+  --directory <directory>  The directory to scan
+  -v                       Increases the verbosity of messages
+  -h, --help               Displays this help message
+
+HELP;
+
+if (array_key_exists('h', $options) || array_key_exists('help', $options)) {
+    echo $help;
+    exit(0);
+}
+
 if ($existenceStatusCode !== 0) {
     echo 'The lpv binary is not available.';
     exit(1);
 }
 
+if (!array_key_exists('directory', $options)) {
+    echo 'The directory to scan has not been provided. See --help.' . PHP_EOL;
+    exit(1);
+}
+
+$directoryToScan = $options['directory'];
+
+if (file_exists($directoryToScan) === false) {
+    echo "The provided directory [$directoryToScan] to scan doesn't exist." . PHP_EOL;
+    exit(1);
+}
+
+if (array_key_exists('v', $options)) {
+    $verbose = true;
+}
+
 if (count($argv) === 1) {
-    echo 'Running lpv validate.' . PHP_EOL;
-    exec("lpv validate", $output, $statusCode);
+    echo "Running lpv validate $directoryToScan." . PHP_EOL;
+    exec("lpv validate $directoryToScan", $output, $statusCode);
     exit($statusCode);
 }
 
-if ($argv[1] === '-v') {
-    $lpvFileCommand = 'lpv validate -v';
-    $lpvGlobFileCommand = 'lpv validate -v --glob-pattern ' . $argv[2];
-} else {
-    $lpvFileCommand = 'lpv validate';
-    $lpvGlobFileCommand = 'lpv validate --glob-pattern ' . $argv[2];
+$lpvCommand = "lpv validate $directoryToScan";
+if (isset($argv[3]) && $argv[3] !== "use-lpv-file") {
+    $lpvCommand = "lpv validate --glob-pattern " . $argv[3] . " $directoryToScan";
 }
 
-if ($argv[2] === "use-lpv-file") {
-    echo 'Running ' . $lpvFileCommand . '.' . PHP_EOL;
-    exec($lpvFileCommand, $output, $statusCode);
-    var_dump($output);
-} else {
-    echo 'Running ' . $lpvGlobFileCommand . PHP_EOL;
-    exec($lpvGlobFileCommand, $output, $statusCode);
+if ($verbose) {
+    $lpvCommand = "lpv validate -v $directoryToScan";
+    if (isset($argv[3]) && $argv[3] !== "use-lpv-file") {
+        $lpvCommand = "lpv validate -v --glob-pattern " . $argv[3] . " $directoryToScan";
+    }
 }
+
+echo 'Running ' . $lpvCommand . '.' . PHP_EOL;
+exec($lpvCommand, $output, $statusCode);
+var_dump($output);
 
 exit($statusCode);
