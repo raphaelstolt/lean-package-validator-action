a5d4f4a8b072378ac2139098dd61705d4cebf460
Raphael Stolt <raphael.stolt@gmail.com>
2023-10-13 12:31:33 +0200

    Reduces opcode conversions

--- bin/lpv-runner.php
+++ bin/lpv-runner.php
@@ -2,14 +2,14 @@
 
 <?php
 
-exec("lpv --version 2> /dev/null", $output, $existenceStatusCode);
+\exec("lpv --version 2> /dev/null", $output, $existenceStatusCode);
 
 if ($existenceStatusCode !== 0) {
     echo 'The lpv binary is not available.' . PHP_EOL;
     exit(1);
 }
 
-$options = getopt('h', ['directory:', 'help']);
+$options = \getopt('h', ['directory:', 'help']);
 $verbose = false;
 
 $help = <<<HELP
@@ -24,42 +24,44 @@ Options:
 
 HELP;
 
-if (array_key_exists('h', $options) || array_key_exists('help', $options)) {
+if (\array_key_exists('h', $options) || \array_key_exists('help', $options)) {
     echo $help;
-    exit(0);
+    \exit(0);
 }
 
-if (!array_key_exists('directory', $options)) {
+if (!\array_key_exists('directory', $options)) {
     echo 'The directory to scan has not been provided. See --help.' . PHP_EOL;
-    exit(1);
+    \exit(1);
 }
 
 $directoryToScan = $options['directory'];
 
-if (file_exists($directoryToScan) === false) {
+if (\file_exists($directoryToScan) === false) {
     echo "The provided directory [$directoryToScan] to scan doesn't exist." . PHP_EOL;
-    exit(1);
+    \exit(1);
 }
 
 $lpvCommand = "lpv validate $directoryToScan";
 
+// verbosity
 if (isset($argv[3]) && $argv[3] == 'true') {
     $lpvCommand = "lpv validate -v $directoryToScan";
 }
 
+// glob pattern or .lpv file in repository
 if (isset($argv[4]) && $argv[4] !== 'use-lpv-file') {
     $lpvCommand.= " --glob-pattern " . trim($argv[4]);
 }
 
 echo 'Running ' . $lpvCommand . '.' . PHP_EOL;
-exec($lpvCommand . ' 2> /dev/null', $output, $statusCode);
+\exec($lpvCommand . ' 2> /dev/null', $output, $statusCode);
 
 foreach ($output as $outputLine) {
     echo $outputLine . PHP_EOL;
 }
 
 if ($statusCode !== 0) {
-    echo 'Running lpv failed.' . PHP_EOL;
+    echo 'Running ' . $lpvCommand .  ' failed.' . PHP_EOL;
 }
 
 exit($statusCode);
